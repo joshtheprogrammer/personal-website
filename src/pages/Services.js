5a5d7c1820c002458d626d9d0d22f25e1c21b479
joshtheprogrammer <joshuahonguyen@gmail.com>
2021-10-19 15:10:16 -0500

    fixes

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,8 +3,8 @@ import React from 'react'
 import "./Pages.css"
 
 const Services = () => {
+    document.title = "Services";
     return (
-        document.title = "Services";
         <div id="services" class="page">
             <a href='https://www.trash2things.com' target="_blank">
                 Trash2Things (coming soon)
