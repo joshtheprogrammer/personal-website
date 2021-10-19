44c368d86441460262a8bf6f2a6493f91106d07d
joshtheprogrammer <joshuahonguyen@gmail.com>
2021-10-19 14:40:28 -0500

    no

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import "./Pages.css"
+
 const Services = () => {
     return (
+        document.title = "Services";
         <div id="services" class="page">
             <a href='https://www.trash2things.com' target="_blank">
                 Trash2Things (coming soon)
